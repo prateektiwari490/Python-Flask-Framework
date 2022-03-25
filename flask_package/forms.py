de705ee74171edcba9cb6c6ca23b3dbb9878f975
Prateek Tiwari <prateektiwari490@gmail.com>
2022-03-25 14:59:20 +0530

    Logged in

--- flask_package/forms.py
+++ flask_package/forms.py
@@ -29,4 +29,4 @@ class LoginForm(FlaskForm):
 	email = StringField('Email', validators=[DataRequired(), Email()])
 	password =  PasswordField('Password', validators = [DataRequired()])
 	remember = BooleanField('Remember Me')
-	submit =  SubmitField("Log In")
+	submit =  SubmitField("Logged In")
